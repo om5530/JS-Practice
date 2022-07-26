acc2df50cadae3ca9aa98e87d64cce616efca79d
Omkar <omkarbaacha@foreignadmits.com>
2022-07-26 21:32:03 +0530

    ..

--- js interview/task2.js
+++
@@ -1,9 +0,0 @@
-//What will be logged in first example and in second example
-
-let val1 ;
-console.log(val1)
-console.log(typeof var1);
-
-let var2 = null
-console.log(var2);
-console.log(typeof var2)
