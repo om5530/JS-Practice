d45624d1642d80abfc13d8d0cdc2210b5a5118e9
Omkar <omkarbacha25@gmail.com>
2022-09-28 13:27:49 +0530

    ..

--- Js Nuggets/17_asyncAwait.js
+++ Js Nuggets/17_asyncAwait.js
@@ -1,69 +1,80 @@
-//Javascript Nuggets
-//async / await
-//async must be present , always  returns a promise
-//await waits till promise is settled,
-//error handling - try/catch block.
+// //Javascript Nuggets
+// //async / await
+// //async must be present , always  returns a promise
+// //await waits till promise is settled,
+// //error handling - try/catch block.
 
-const example = async () => {
-  return 'Hello There';
-};
+// const example = async () => {
+//   return 'Hello There';
+// };
 
-async function somFunction() {
-  const result = await example();
-  // console.log(result);
+// async function somFunction() {
+//   const result = await example();
+//   // console.log(result);
+// }
+
+// //  console.log(example())
+// somFunction(); //invoking the function
+
+// const users = [
+//   { id: 1, name: 'john' },
+//   { id: 2, name: 'susan' },
+//   { id: 3, name: 'anna' }, 
+// ];
+
+// const articles = [
+//   { userId: 1, articles: ['one', 'two', 'three'] },
+//   { userId: 2, articles: ['four', 'five'] },
+//   { userId: 3, articles: ['six', 'seven', 'eight'] },
+// ];
+
+// const getData = async ()=>{
+//   try{
+//   const user = await getUser('john')
+//   // console.log(user)
+//   if(user){
+//    const  articles = await getArticles(user.id)
+//    console.log(articles)
+//   }
+// } catch(err){
+//    console.log(err)
+// }
+// }
+
+// getData();
+// // getUser('susan')
+// //   .then((user) => getArticles(user.id)).then((articles)=> console.log(articles ))
+// //   .catch((err) => console.log(err));
+
+// function getUser(name) {
+//   return new Promise((resolve, reject) => {
+//     const user = users.find((user) => user.name === name);
+//     if (user) {
+//       return resolve(user);
+//     } else {
+//       reject(`No such user with name :${name}`);
+//     }
+//   });
+// }
+
+// function getArticles(userId) {
+//   return new Promise((resolve, reject) => {
+//     const userArticles = articles.find((user) => user.userId === userId);
+//     if (userArticles) {
+//       return resolve(userArticles.articles);
+//     } else {
+//       reject('Wrong ID');
+//     }
+//   });
+// }
+
+
+class Sai{
+  constructor(name,lastNmae){
+    this.name =name
+    this.lastNmae = lastNmae
+   }
 }
 
-//  console.log(example())
-somFunction(); //invoking the function
-
-const users = [
-  { id: 1, name: 'john' },
-  { id: 2, name: 'susan' },
-  { id: 3, name: 'anna' }, 
-];
-
-const articles = [
-  { userId: 1, articles: ['one', 'two', 'three'] },
-  { userId: 2, articles: ['four', 'five'] },
-  { userId: 3, articles: ['six', 'seven', 'eight'] },
-];
-
-const getData = async ()=>{
-  try{
-  const user = await getUser('john')
-  // console.log(user)
-  if(user){
-   const  articles = await getArticles(user.id)
-   console.log(articles)
-  }
-} catch(err){
-   console.log(err)
-}
-}
-
-getData();
-// getUser('susan')
-//   .then((user) => getArticles(user.id)).then((articles)=> console.log(articles ))
-//   .catch((err) => console.log(err));
-
-function getUser(name) {
-  return new Promise((resolve, reject) => {
-    const user = users.find((user) => user.name === name);
-    if (user) {
-      return resolve(user);
-    } else {
-      reject(`No such user with name :${name}`);
-    }
-  });
-}
-
-function getArticles(userId) {
-  return new Promise((resolve, reject) => {
-    const userArticles = articles.find((user) => user.userId === userId);
-    if (userArticles) {
-      return resolve(userArticles.articles);
-    } else {
-      reject('Wrong ID');
-    }
-  });
-}
+let x = new Sai('sai','rathod') 
+console.log(x)
---
+++ Js Nuggets/mapVsFilter.js
@@ -0,0 +1,24 @@
+let arr1 = [ 1,2,4,6,7 ] ;
+let arr2 = arr1.filter ( element => element > 4 ) ;
+
+console.log(arr2)
+console.log(arr1)
+
+
+let arr3 = [ 1,2,3,4,5 ] ;
+let arr4 = arr3.map ( element =>{
+return element   }) ;
+console.log( arr4 ) ;
+
+
+let sum = 0
+let arr5 = [ 1,2,3,4,5 ] ;
+let arr6 = arr5.forEach ( element =>
+ element  );
+console.log(arr6);
+
+// for(let ele of arr5){
+//   return ele
+// }
+let x = 10
+console.log(++x)
